40d6ab0016a9d7cbf4fd24f8cb0d0987379489e1
Andy Kong <andy.kong@slalom.com>
2020-02-14 12:15:27 -0600

    add aws_main_route_table association

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,6 +51,11 @@ resource "aws_route" "public_internet_gateway_ipv6" {
   gateway_id                  = aws_internet_gateway.gw.id
 }
 
+resource "aws_main_route_table_association" "a" {
+  vpc_id         = aws_vpc.blackslope_main.id
+  route_table_id = aws_route_table.public[0].id
+}
+
 resource "aws_subnet" "blackslope_main-public-1" {
   availability_zone       = "${var.aws_region}a"
   vpc_id                  = aws_vpc.blackslope_main.id
